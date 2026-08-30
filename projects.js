5c433d3bfd0916138b7c0a825e6b26da57eb0fc1
Sophezho <sophezho@gmail.com>
2026-08-30 05:06:56 +0500

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,76 +1,79 @@
 window.PORTFOLIO_GALLERIES = [
   {
-    "key": "illustration",
-    "title": "Illustration & Music Art",
-    "items": [
-      {
-        "src": "assets/illustration/CompMusic1.jpg",
-        "alt": "CompMusic1"
-      },
-       {
-        "src": "assets/illustration/CompMusic2.jpg",
-        "alt": "CompMusic2"
-      },
-       {
-        "src": "assets/illustration/CompMusic3.jpg",
-        "alt": "CompMusic3"
-      },
-      {
-        "src": "assets/illustration/illustration-18.jpg",
-        "alt": "Illustration 18"
-      },
-      {
-        "src": "assets/illustration/Comp5.jpg",
-        "alt": "Comp5"
-      }
-    ]
-  },
- {
-    "key": "graphic",
-    "title": "Graphic Design",
-    "items": [
-      {
-        "src": "assets/graphic/Apparel Design.jpg",
-        "alt": "Apparel Design.jpg"
-      },
-      {
-        "src": "assets/graphic/GraphCompilation_0.jpg",
-        "alt": "GraphCompilation_0.jpg"
-      },
-      {
-        "src": "assets/graphic/GraphCompilation_3.jpg",
-        "alt": "GraphCompilation_3.jpg"
-      },
-      {
-        "src": "assets/graphic/GraphCompilation_4.jpg",
-        "alt": "GraphCompilation_4.jpg"
-      }
-    ]
+        "key": "illustration",
+        "title": "Illustration & Music Art",
+        "cover": "assets/cover-illustration.jpg",
+        "items": [
+          {
+                    "src": "assets/illustration/CompMusic1.jpg",
+                    "alt": "CompMusic1"
+          },
+          {
+                    "src": "assets/illustration/CompMusic2.jpg",
+                    "alt": "CompMusic2"
+          },
+          {
+                    "src": "assets/illustration/CompMusic3.jpg",
+                    "alt": "CompMusic3"
+          },
+          {
+                    "src": "assets/illustration/illustration-18.jpg",
+                    "alt": "Illustration 18"
+          },
+          {
+                    "src": "assets/illustration/Comp5.jpg",
+                    "alt": "Comp5"
+          }
+              ]
   },
   {
-    "key": "murals",
-    "title": "Street Art & Murals",
-    "items": [
-      {
-        "src": "assets/murals/MuralComp_3.jpg",
-        "alt": "MuralComp_3"
-      },
-      {
-        "src": "assets/murals/murals-05.jpg",
-        "alt": "Mural 5"
-      },
-      {
-        "src": "assets/murals/murals-04.jpg",
-        "alt": "Mural 4"
-      },
-     {
-        "src": "assets/murals/MuralComp_2.jpg",
-        "alt": "MuralComp_2"
-      },
-       {
-        "src": "assets/murals/MuralComp_1.jpg",
-        "alt": "MuralComp_1"
-      }
-    ]
+        "key": "graphic",
+        "title": "Graphic Design",
+        "cover": "assets/cover-graphic.jpg",
+        "items": [
+          {
+                    "src": "assets/graphic/Apparel Design.jpg",
+                    "alt": "Apparel Design.jpg"
+          },
+          {
+                    "src": "assets/graphic/GraphCompilation_0.jpg",
+                    "alt": "GraphCompilation_0.jpg"
+          },
+          {
+                    "src": "assets/graphic/GraphCompilation_3.jpg",
+                    "alt": "GraphCompilation_3.jpg"
+          },
+          {
+                    "src": "assets/graphic/GraphCompilation_4.jpg",
+                    "alt": "GraphCompilation_4.jpg"
+          }
+              ]
+  },
+  {
+        "key": "murals",
+        "title": "Street Art & Murals",
+        "cover": "assets/cover-murals.jpg",
+        "items": [
+          {
+                    "src": "assets/murals/MuralComp_3.jpg",
+                    "alt": "MuralComp_3"
+          },
+          {
+                    "src": "assets/murals/murals-05.jpg",
+                    "alt": "Mural 5"
+          },
+          {
+                    "src": "assets/murals/murals-04.jpg",
+                    "alt": "Mural 4"
+          },
+          {
+                    "src": "assets/murals/MuralComp_2.jpg",
+                    "alt": "MuralComp_2"
+          },
+          {
+                    "src": "assets/murals/MuralComp_1.jpg",
+                    "alt": "MuralComp_1"
+          }
+              ]
   }
-];
+  ];
